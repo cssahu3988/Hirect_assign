7bf77353ce04aa09cb9aa468406ff568288fc36a
Chandra Sekhar Sahu <cssahu3988@gmail.com>
2022-02-27 11:54:25 +0530

    c2

--- app/src/main/java/com/example/hirect_assign/MainActivity.java
+++ app/src/main/java/com/example/hirect_assign/MainActivity.java
@@ -1,94 +1,68 @@
 package com.example.hirect_assign;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.ViewGroup;
+import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.hirect_assign.databinding.ActivityMainBinding;
-import com.example.hirect_assign.databinding.CrptoItemBinding;
+import com.example.hirect_assign.fragments.Fragment1;
+import com.example.hirect_assign.fragments.Fragment2;
 import com.example.hirect_assign.pojo.Model1;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private ArrayList<Model1> list = new ArrayList<>();
+    private FragmentTransaction transaction;
+    private Fragment1 fragment1 = null;
+    private Fragment2 fragment2 = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        LoadData();
-        SetUpRecyclerView();
+        SetUpFragments();
     }
 
-    private void LoadData() {
-        Model1 m = new Model1("BTC","+1.6%", "$ 29,850.15", "1 BTC","",
-                getDrawable(R.drawable.bitcoin),getDrawable(R.drawable.bit_coin),getDrawable(R.drawable.bit_coin_),
-                null, Color.parseColor("#F6543E"));
-        Model1 m2 = new Model1("ETH","-0.82%", "$ 10,561.24", "1 ETH","",
-                getDrawable(R.drawable.etherium),getDrawable(R.drawable.eth),getDrawable(R.drawable.eth_),
-                null, Color.parseColor("#6374C3"));
-        Model1 m3 = new Model1("USDT","-2.10%", "$ 3,676.76", "1 LTC","",
-                getDrawable(R.drawable.tcoin),getDrawable(R.drawable.usdt),getDrawable(R.drawable.usdt_),
-                null, Color.parseColor("#30E0A1"));
-        Model1 m4 = new Model1("BNC","+0.27%", "$ 5,241.62", "1 BNC","",
-                getDrawable(R.drawable.bnc),getDrawable(R.drawable.bnc2),getDrawable(R.drawable.bnc2_),
-                null, Color.parseColor("#F3BA2F"));
-        Model1 m5 = new Model1("ICP","+0.27%", "$ 5,241.62", "1 ICP","",
-                getDrawable(R.drawable.infinity),getDrawable(R.drawable.icp),getDrawable(R.drawable.icp_),
-                null, Color.parseColor("#B7217D"));
-
-
-        list.add(m);
-        list.add(m2);
-        list.add(m3);
-        list.add(m4);
-        list.add(m5);
+    private void SetUpFragments(){
+        transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.frameLayout,getFragment1(),"fragment1").commit();
+        binding.bottomNavBar.setOnNavigationItemSelectedListener(selectedListener);
     }
 
-    private void SetUpRecyclerView() {
-        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        binding.recyclerView.addItemDecoration(new DividerItemDecoration(this,
-                DividerItemDecoration.VERTICAL));
-        MyAdapter adapter = new MyAdapter();
-        binding.recyclerView.setAdapter(adapter);
-        adapter.notifyDataSetChanged();
-    }
-
-
-    private class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
-
-        @NonNull
+    private BottomNavigationView.OnNavigationItemSelectedListener selectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
-        public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            CrptoItemBinding binding = DataBindingUtil.inflate(getLayoutInflater(),R.layout.crpto_item,parent,false);
-            return new ViewHolder(binding);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-            holder.binding.setModel(list.get(position));
-        }
-
-        @Override
-        public int getItemCount() {
-            return list.size();
-        }
-
-        public class ViewHolder extends RecyclerView.ViewHolder {
-            CrptoItemBinding binding;
-            public ViewHolder(@NonNull CrptoItemBinding itemBinding) {
-                super(itemBinding.getRoot());
-                this.binding = itemBinding;
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            transaction = getSupportFragmentManager().beginTransaction();
+            switch (item.getItemId()){
+                case R.id.home:
+                    transaction.replace(R.id.frameLayout,getFragment1(),"fragment1");
+                    break;
+                case R.id.attachi:
+                    transaction.replace(R.id.frameLayout,getFragment2(),"fragment2");
+                    break;
             }
+            transaction.commit();
+            return true;
         }
+    };
+
+    private Fragment1 getFragment1(){
+        if (fragment1==null){
+            fragment1 = new Fragment1();
+        }
+        return fragment1;
     }
 
+    public Fragment2 getFragment2() {
+        if(fragment2==null){
+            fragment2 = new Fragment2();
+        }
+        return fragment2;
+    }
 }
---
+++ app/src/main/java/com/example/hirect_assign/fragments/Fragment2.java
@@ -0,0 +1,104 @@
+package com.example.hirect_assign.fragments;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.hirect_assign.R;
+import com.example.hirect_assign.databinding.CrptoItemBinding;
+import com.example.hirect_assign.databinding.Fragment2Binding;
+import com.example.hirect_assign.pojo.Model1;
+
+import java.util.ArrayList;
+
+public class Fragment2 extends Fragment {
+
+    private Fragment2Binding binding;
+    private ArrayList<Model1>list = new ArrayList<>();
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LoadData();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_2,container,false);
+    }
+
+
+    private void LoadData() {
+        list.clear();
+        Model1 m = new Model1("BTC","+1.6%", "$ 29,850.15", "1 BTC","",
+                getActivity().getDrawable(R.drawable.bitcoin),getActivity().getDrawable(R.drawable.bit_coin),getActivity().getDrawable(R.drawable.bit_coin_),
+                null, Color.parseColor("#F6543E"));
+        Model1 m2 = new Model1("ETH","-0.82%", "$ 10,561.24", "1 ETH","",
+                getActivity().getDrawable(R.drawable.etherium),getActivity().getDrawable(R.drawable.eth),getActivity().getDrawable(R.drawable.eth_),
+                null, Color.parseColor("#6374C3"));
+        Model1 m3 = new Model1("USDT","-2.10%", "$ 3,676.76", "1 LTC","",
+                getActivity().getDrawable(R.drawable.tcoin),getActivity().getDrawable(R.drawable.usdt),getActivity().getDrawable(R.drawable.usdt_),
+                null, Color.parseColor("#30E0A1"));
+        Model1 m4 = new Model1("BNC","+0.27%", "$ 5,241.62", "1 BNC","",
+                getActivity().getDrawable(R.drawable.bnc),getActivity().getDrawable(R.drawable.bnc2),getActivity().getDrawable(R.drawable.bnc2_),
+                null, Color.parseColor("#F3BA2F"));
+        Model1 m5 = new Model1("ICP","+0.27%", "$ 5,241.62", "1 ICP","",
+                getActivity().getDrawable(R.drawable.infinity),getActivity().getDrawable(R.drawable.icp),getActivity().getDrawable(R.drawable.icp_),
+                null, Color.parseColor("#B7217D"));
+
+
+        list.add(m);
+        list.add(m2);
+        //list.add(m3);
+        //list.add(m4);
+        //list.add(m5);
+    }
+
+    private void SetUpRecyclerView() {
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        binding.recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(),
+                DividerItemDecoration.VERTICAL));
+        MyAdapter adapter = new MyAdapter();
+        binding.recyclerView.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+    }
+
+    private class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder>{
+
+        @NonNull
+        @Override
+        public MyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            CrptoItemBinding binding = DataBindingUtil.inflate(getLayoutInflater(),R.layout.crpto_item,parent,false);
+            return new ViewHolder(binding);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyAdapter.ViewHolder holder, int position) {
+            holder.binding.setModel(list.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            CrptoItemBinding binding;
+            public ViewHolder(@NonNull CrptoItemBinding itemBinding) {
+                super(itemBinding.getRoot());
+                this.binding = itemBinding;
+            }
+        }
+    }
+
+
+}
